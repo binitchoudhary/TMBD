a99cbb817b795fa02731c6bd4d99c5fd1c107005
binitchoudhary <binitchoudhary1602@gmail.com>
2022-05-23 17:56:07 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+//TMBD
+
+const API_KEY = 'api_key=661025e2269937452191dad4bf9cf7ff';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' +API_KEY;
+const IMG_URl = 'https://image.tmdb.org/t/p/w500';
+const main = document.getElementById('main');
+const form = document.getElementById("form");
+const search = document.getElementById('search');
+const searchURL = BASE_URL + "/search/movie?"+API_KEY;
+
+
+getmovies(API_URL);
+
+function getmovies(url){
+
+    fetch(url).then(res => res.json()).then(data => {
+        console.log(data.results)
+         showMovies(data.results);
+    })
+}
+
+function showMovies(data){
+    main.innerHTML= '';
+
+    data.forEach(movie => {
+        const {title, poster_path, vote_average, overview} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML =`
+        <img src="${IMG_URl+poster_path}" alt="${title}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getColor(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+            <h3>overview</h3>
+            ${overview}
+        </div>`
+
+        main.appendChild(movieEl)
+    });
+}
+
+
+function getColor(vote){
+if(vote>=8){
+    return 'green'
+}else if(vote>=5){
+return 'orange'
+}else{
+    return 'red'
+}
+}
+
+
+form.addEventListener('submit', (e)=> {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+    
+    if(searchTerm){
+        getmovies(searchURL + '&query='+searchTerm);
+    }else{
+        getmovies(API_URL);
+    }
+})
